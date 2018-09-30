608da94be5faaaf4723ee00d95465f6d98112d51
voltasaur <volkozavr7@gmail.com>
2018-09-30 13:11:11 -0700

    added 3 new files h_work

---
+++ h_work_7.1.py
@@ -0,0 +1,15 @@
+pH = float(input("Enter the requested pH: "))
+
+def answer_pH(val):
+
+	if val == 3.0 :
+		print ("Apple juice.", val, "pH")
+	elif val == 5.5:
+		print ("Shampoo.", val, "pH")
+	elif val == 9.0:
+		print ("Hand soap.", val, "pH")
+	else:
+		print ("Enter error.")
+
+answer_pH(pH)
+	
